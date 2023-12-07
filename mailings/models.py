0773e2eae5680a4a02b07c8b4b01453d130d6a0f
JL34 <johnny2791@seznam.cz>
2023-12-07 11:14:00 +0100

    created models.py for Client, MailingList, MailingMessage, MailingLog

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+# pip3 install psycopg2-binary
+# py manage.py migrate
+# py manage.py makemigrations
+
+class Client(models.Model):
+    email = models.EmailField(verbose_name="E-mail")
+    name = models.CharField(max_length=64, verbose_name="Name")
+    surname = models.CharField(max_length=64, verbose_name="Surname")
+    comment = models.TextField(verbose_name="Comment")
+
+    def __str__(self):
+        return f"{self.name} {self.surname} --- {self.email}"
+
+    class Meta:
+        verbose_name = "Client"
+        verbose_name_plural = "Clients"
+        ordering = ("email",)
+
+
+class MailingList(models.Model):
+    PERIODICITY_CHOICES = [  # CapsLock
+        ("daily", "Once a day"),  # Prvním prvkem každého tuplu je skutečná hodnota,
+        ("weekly", "Once a week"),  # která má být nastavena v modelu,
+        ("monthly", "Once a month")  # a druhým prvkem je lidsky čitelný název.
+    ]
+
+    mailing_time = models.TimeField(verbose_name="Mailing time")
+    periodicity = models.CharField(max_length=12, choices=PERIODICITY_CHOICES, verbose_name="Periodicity")
+    mailing_status = models.CharField(max_length=24, verbose_name="Mailing status")
+
+    class Meta:
+        verbose_name = "Mailing list"
+        verbose_name_plural = "Mailign lists"
+
+
+class MailingMessage(models.Model):
+    mailing_list = models.ForeignKey(MailingList,  # pokud je odstraněn MailingList, měly by být odstraněny
+                                     on_delete=models.CASCADE)  # i všechny související zprávy v MailingMessage.
+    subject = models.CharField(max_length=256, verbose_name="E-mail subject")
+    body = models.TextField(verbose_name="E-mail body")
+
+    class Meta:
+        verbose_name = "Mailing message"
+        verbose_name_plural = "Mailing messages"
+
+
+class MailingLog(models.Model):
+    mailing_list = models.ForeignKey(MailingList, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(default=timezone.now, verbose_name="Date & Time")
+    status = models.CharField(max_length=24, verbose_name="Status")
+    server_response = models.TextField(verbose_name="Server response")
+
+    class Meta:
+        verbose_name = "Mailing log"
+        verbose_name_plural = "Mailing logs"
