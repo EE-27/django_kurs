1b7f479bc303ab5a201c6233755e1083e96e2d60
JL34 <johnny2791@seznam.cz>
2023-12-11 14:44:22 +0100

    made base, to make it easier to create new html files

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from mailings.views import ClientListView
+from mailings.views import ClientListView, index, ClientDetailView
 
 urlpatterns = [
-    path("", ClientListView.as_view(), name='index'),
+    path("", index),
+    path("client_list_view/", ClientListView.as_view(), name='client_list_view'),
+    path("client_detail/<int:pk>/", ClientDetailView.as_view(), name="client_detail")
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from mailings.models import Client
 
 
 # Create your views here.
 
-# def index(request):
-#     return render(request, "mailings/index.html")
+def index(request):
+    return render(request, "mailings/index.html")
 
 
 class ClientListView(ListView):
     model = Client
-    template_name = 'mailings/index.html'
+    template_name = 'mailings/client_list_view.html'
+
+
+class ClientDetailView(DetailView):
+    model = Client
+    template_name = "mailings/client_detail.html"
